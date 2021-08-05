9adc52e13e674becb5fba887a5f8095a19b6e456
Jhon Calderón <jhoncv@digitalware.com.co>
2021-08-05 17:14:13 -0500

    adding folder structure

---
+++ JAC.MusicVideoList.Application.Main/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace JAC.MusicVideoList.Application.Main
+{
+    public class Class1
+    {
+    }
+}
---
+++ JAC.MusicVideoList.Domain.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace JAC.MusicVideoList.Domain.Core
+{
+    public class Class1
+    {
+    }
+}
